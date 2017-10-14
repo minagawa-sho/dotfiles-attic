740c36e3b0a020b7887233bac79984b607d7cb03
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 04:06:45 +0900

    Do fzf startup

---
+++ lib/fzf_startup.sh
@@ -0,0 +1,9 @@
+# pip_startup.sh
+# shellcheck disable=SC1090
+# shellcheck disable=SC2148
+
+# Load fzf settings
+[ -f ~/.fzf.bash ] && . ~/.fzf.bash
+
+# Alias fzf as 'igrep'
+alias igrep="fzf --select-1 --exit-0 --query"
