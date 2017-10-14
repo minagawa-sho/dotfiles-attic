7e2f55bf052ff05ee47f7d98350c01e7c1ab5c04
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 03:38:49 +0900

    Do pip startup

---
+++ lib/pip_startup.sh
@@ -0,0 +1,5 @@
+# pip.sh
+# shellcheck disable=SC2148
+
+# Load bash completion for pip
+eval "`pip completion --bash`"
--- libexec/startup.sh
+++ libexec/startup.sh
@@ -1,4 +1,7 @@
 # startup.sh
+# shellcheck disable=SC1090
 # shellcheck disable=SC2148
 
 export DOTFILESPATH="$HOME/.dotfiles"
+
+. $DOTFILESPATH/lib/pip_startup.sh
