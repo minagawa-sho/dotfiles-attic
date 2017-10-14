9b2d4dadb6eb0d35ebae266a205c7ddb469f382b
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 02:31:48 +0900

    Install dotfiles with zip at first and replace to symlink later

--- bin/install.sh
+++ bin/install.sh
@@ -3,15 +3,24 @@
 
 set -e
 
+# Define constants
+dotfiles_path="$HOME/.dotfiles"
+dotfiles_config="$HOME/.dotfilesrc"
+dotfiles_zipurl='https://codeload.github.com/minagoro/dotfiles-attic/zip/master'
+
 # Load envs as dotfiles config
-. $HOME/.dotfilesrc
+. $dotfiles_config
 
-# Clone dotfiles repository
-# TODO: Require ghq installation
-ghq get $DOTFILESPROJ
+# Install dotfiles as ~/.dotfiles
+curl -LsS $dotfiles_zipurl > $HOME/dotfiles-attic-master.zip
+unzip $HOME/dotfiles-attic-master.zip -d $HOME
+mv $HOME/dotfiles-attic-master $dotfiles_path
 
-# Symlink ~/.dotfiles to the working directory
-(
-  dotfilesdir=`ghq list --exact --full-path $DOTFILESPROJ`
-  ln -snf $dotfilesdir $HOME/.dotfiles
-)
+# Install all apps and config files
+# TODO: impl
+
+# Replace ~/.dotfiles to the working directory symlink
+ghq list | grep $DOTFIELSPROJ || ghq get $DOTFILESPROJ
+rm -rf $dotfiles_path
+dotfilesdir=`ghq list --exact --full-path $DOTFILESPROJ`
+ln -snf $dotfilesdir $dotfiles_path
