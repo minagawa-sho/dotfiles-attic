1b1575b066cf5f0662f87064bb83254625027964
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 04:51:57 +0900

    Define ls aliases on startup

---
+++ lib/ls_startup.sh
@@ -0,0 +1,7 @@
+# less_startup.sh
+# shellcheck disable=SC2148
+
+# Alias for abbrebiations
+alias ls='ls -G'
+alias ll='ls -l'
+alias la='ll -a'
--- libexec/startup.sh
+++ libexec/startup.sh
@@ -4,13 +4,13 @@
 
 export DOTFILESPATH="$HOME/.dotfiles"
 
-# Python development
 . $DOTFILESPATH/lib/blueprints_startup.sh
 . $DOTFILESPATH/lib/builtin_startup.sh
 . $DOTFILESPATH/lib/fzf_startup.sh
 . $DOTFILESPATH/lib/ghq_startup.sh
 . $DOTFILESPATH/lib/go_startup.sh
 . $DOTFILESPATH/lib/locate_startup.sh
+. $DOTFILESPATH/lib/ls_startup.sh
 . $DOTFILESPATH/lib/pip_startup.sh
 . $DOTFILESPATH/lib/pyenv_startup.sh
 . $DOTFILESPATH/lib/system_startup.sh
