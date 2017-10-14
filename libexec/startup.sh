b86e3ee0efc257fe8d6dc10167d754d2ad556a21
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 04:49:52 +0900

    Rename lib/system_startup.sh to lib/builtin_startup.sh

--- libexec/startup.sh
+++ libexec/startup.sh
@@ -6,6 +6,7 @@ export DOTFILESPATH="$HOME/.dotfiles"
 
 # Python development
 . $DOTFILESPATH/lib/blueprints_startup.sh
+. $DOTFILESPATH/lib/builtin_startup.sh
 . $DOTFILESPATH/lib/fzf_startup.sh
 . $DOTFILESPATH/lib/ghq_startup.sh
 . $DOTFILESPATH/lib/go_startup.sh
