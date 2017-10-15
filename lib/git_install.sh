dc05d3760eb5c44551942c166f680789014f7f69
Sho Minagawa <minagoro0522@gmail.com>
2017-10-15 11:49:10 +0900

    Fix :bug: so that config addition with --global

--- lib/git_install.sh
+++ lib/git_install.sh
@@ -11,18 +11,18 @@ which git || brew install git
 brew upgrade
 
 # Configurate app
-git config --unset-all
-git config --add user.name 'Sho Minagawa'
-git config --add user.email minagoro0522@gmail.com
-#git config --add user.signingkey XXXXXXXX
-git config --add alias.co checkout
-git config --add alias.st status
-git config --add alias.ci commit
-git config --add alias.br branch
-git config --add alias.cm commit
-git config --add alias.unstage 'reset HEAD --'
-git config --add alias.last 'log -1 HEAD'
-git config --add alias.visual '!gitk'
-#git config --add alias.ignore !gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi
-#git config --add alias.license=!gl() { curl --header "Accept: application/vnd.github.drax-preview+json" -XGET https://api.github.com/licenses/$@ | jq ".body";}; gl
-#git config --add core.excludesfile /Users/minagawa-sho/.gitignore/Global/macOS.gitignore
+rm -f $HOME/.gitconfig
+git config --global --add user.name 'Sho Minagawa'
+git config --global --add user.email minagoro0522@gmail.com
+#git config --global --add user.signingkey XXXXXXXX
+git config --global --add alias.co checkout
+git config --global --add alias.st status
+git config --global --add alias.ci commit
+git config --global --add alias.br branch
+git config --global --add alias.cm commit
+git config --global --add alias.unstage 'reset HEAD --'
+git config --global --add alias.last 'log -1 HEAD'
+git config --global --add alias.visual '!gitk'
+#git config --global --add alias.ignore !gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi
+#git config --global --add alias.license=!gl() { curl --header "Accept: application/vnd.github.drax-preview+json" -XGET https://api.github.com/licenses/$@ | jq ".body";}; gl
+#git config --global --add core.excludesfile /Users/minagawa-sho/.gitignore/Global/macOS.gitignore
